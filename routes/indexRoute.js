89edbbffdfedb33923cb32d16e0a5e5699a4b00b
Yazeed aljuhani <yazeedmalmarwani@gmail.com>
2021-02-15 23:16:48 +0300

    mongoDB

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -1,11 +1,9 @@
 const express = require ('express')
 const router = express.Router()
 const blog = require('./blogRoute')
-const user = require('./userRoute')
 
 // add your routes here  👇👇
 
 router.use('/blog', blog) 
-router.use('/user', user)
 
 module.exports = router; 
